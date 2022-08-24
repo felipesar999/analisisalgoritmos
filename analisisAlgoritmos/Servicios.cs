f81f0d2612f7a97dde5a5ce86a835c0606979959
felipesar999 <felipesar999@gmail.com>
2022-08-23 23:43:24 -0500

    clase quiz 7%

---
+++ analisisAlgoritmos/Servicios.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Diagnostics;
+
+namespace analisisAlgoritmos
+{
+    public class Servicios
+    {
+        private int Ciclos { get; set; }
+        private bool ClientNotify { get; set; }
+
+        /*private string Password { get; set; }
+        private string DataBase { get; set; }
+        private string UnitsRange { get; set; }
+        private string ShopId { get; set; }
+        */
+        public Servicios(int ciclos, bool clientNotify = false)
+        {
+            this.Ciclos = ciclos;
+            /*this.User = user;
+            this.Password = pwd;
+            this.ShopId = shopId;
+            this.Token = token;
+            this.DataBase = dataBase;
+            this.UnitsRange = unitsRange;*/
+            this.ClientNotify = clientNotify;
+        }
+
+        public static List<dynamic> ThreeSum(List<Object> a)
+        {
+            List<dynamic> result = new List<dynamic>();
+
+            Stopwatch timer1 = new Stopwatch();
+
+            
+            int N = a.Count;
+            int cnt = 0;
+            Console.WriteLine(a);
+            for (int i=0; i < N; i++)
+            {
+                timer1.Start();
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        if (a[i] + a[j] + a[k] == 0)
+                        {
+                            cnt++;
+                        }
+                        timer1.Stop();
+                        var time = timer1.ElapsedMilliseconds;
+                        result.Add(time);
+
+                    }
+
+                }
+                timer1.Restart();
+            }
+            return result;
+        }
+
+        public static bool TwoSumFast()
+        {
+
+            return true;
+        }
+        public static bool ThreeSumFast()
+        {
+
+            return true;
+        }
+
+    }
+}
---
+++ analisisAlgoritmos/tiempos_Ejecucion.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Text;
+using analisisAlgoritmos;
+
+
+
+public class Ejemplo
+{
+    public static void Main()
+    {
+        Console.WriteLine("Por favor digite el algoritmo a trabajar");
+        int algoritmo = Convert.ToInt32(Console.ReadLine());
+
+        var iteracciones = new List<dynamic>();
+
+        iteracciones.Add(250);
+        iteracciones.Add(500);
+        iteracciones.Add(1000);
+        iteracciones.Add(2000);
+        iteracciones.Add(4000);
+        iteracciones.Add(8000);
+
+
+        if (algoritmo == 1)
+        {
+            Console.WriteLine("aqui");
+            var response = Servicios.ThreeSum(iteracciones);
+        }
+        else if (algoritmo == 2)
+        {
+            var response = Servicios.TwoSumFast();
+        }
+        else if (algoritmo == 3)
+        {
+            var response = Servicios.ThreeSumFast();
+        }
+
+        Stopwatch timer1 = new Stopwatch();
+
+        int cantidad = 0;
+        Console.Write("Digite la cantidad de caracteres a trabajar ");
+        cantidad = Convert.ToInt32(Console.ReadLine());
+
+        string string1 = "", string11 = "", string2 = "", string21 = "", string3 = "", string31 = "";
+
+        string11 = "1";        
+        string1 = string11.PadRight(cantidad, '0');        
+
+        /*Console.Write("Introduce el primer número: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Introduce el segundo número: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+        suma = primerNumero + segundoNumero;
+
+        Console.WriteLine("La suma de {0} y {1} es {2}",
+            primerNumero, segundoNumero, suma);*/
+        timer1.Start();
+        String reversar = String.Empty;
+        char[] reverse = string1.ToCharArray();
+        for (int i = reverse.Length - 1; i > -1; i--)
+        {
+            reversar += reverse[i];
+        }
+        timer1.Stop();
+        Console.WriteLine("El string 1 " +  reversar + " demoro " + timer1.ElapsedMilliseconds + " milisegundos");
+
+        timer1.Restart();
+        string reverse2 = "";
+        Console.Write("Introduce el string2: ");
+        try
+        {
+            string2 = Console.ReadLine();
+        }
+        catch (Exception e) { };
+        foreach (char c in string2)
+        {
+            reverse2 = c + reverse2;
+        }
+        timer1.Stop();
+        Console.WriteLine($"El string 2 : {reverse2} demoro {timer1.ElapsedMilliseconds} milisegundos");
+
+        timer1.Restart();
+        Console.Write("Enter a String3 : ");
+        string3 = Console.ReadLine();
+        char[] reverse3 = string3.ToCharArray();
+        Array.Reverse(reverse3);
+        string reverse31 = new string(reverse3);
+        timer1.Stop();
+        Console.WriteLine($"Reverse String is : {reverse31} demoro {timer1.ElapsedMilliseconds} milisegundos\"");
+        Console.ReadLine();
+
+    }
+}
